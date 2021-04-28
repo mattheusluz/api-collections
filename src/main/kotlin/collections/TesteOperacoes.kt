5955f5f1dfa913a2a9b084b64be083c319f739ab
mattheusluz <mattheusluzz@gmail.com>
2021-04-28 14:08:08 -0300

    Novas operações com array

--- src/main/kotlin/collections/TesteOperacoes.kt
+++ src/main/kotlin/collections/TesteOperacoes.kt
@@ -14,4 +14,14 @@ fun main() {
     println("------------")
     val filtroSalarios = salarios.filter { it >= 4500.00 }
     filtroSalarios.sorted().forEach{ println(it)}       // colocando em ordem crescente
+
+    println("------------")
+    println(salarios.count{it in 4000.00..6000.00})
+
+    println("------------")
+    println(salarios.find { it == 4500.00 })        //procura pelo valor no array e mostra quando acha
+    println(salarios.find { it == 500.00 })         //se não acha retorna null
+
+    println("------------")
+    println(salarios.any { it == 5000.00})          //procura pelo valor e retorna true ou false
 }
