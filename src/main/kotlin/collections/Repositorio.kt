1a47a0c7045342e8e2f9ddc74e2608e95ba865fa
mattheusluz <mattheusluzz@gmail.com>
2021-04-29 11:26:11 -0300

    Ajuste no repositório mutableMap

--- src/main/kotlin/collections/Repositorio.kt
+++ src/main/kotlin/collections/Repositorio.kt
@@ -4,7 +4,7 @@ class Repositorio<T> {
     private val map = mutableMapOf<String, T>()
 
     fun create(id: String, value: T) {
-        map[id] = value
+        map[id] = value      //metodo put melhorado pela sintaxe da IDE
     }
 
     fun remove(id: String) = map.remove(id)
